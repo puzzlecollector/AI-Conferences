e134b14150b8fd1d7d4893148bd79865df45b3f0
Luke Kim <puzzlecollector1123581321@gmail.com>
2023-02-14 17:46:28 +0900

    Add files via upload

---
+++ get_TS2Vec_train.py
@@ -0,0 +1,188 @@
+import numpy as np 
+import pandas as pd 
+import json 
+import ccxt 
+import seaborn as sns
+import os 
+import pandas_ta as ta 
+import time
+from datetime import datetime, timedelta
+import math
+from tqdm.auto import tqdm 
+import matplotlib.pyplot as plt 
+from transformers import * 
+import torch 
+from torch import Tensor 
+from torch.utils.data import * 
+import torch.nn as nn 
+import torch.nn.functional as F 
+from sklearn.utils.class_weight import compute_class_weight 
+from sklearn.metrics import f1_score
+from imblearn.under_sampling import RandomUnderSampler
+from pytorch_metric_learning import miners, losses
+from pytorch_metric_learning.distances import CosineSimilarity
+from scipy.spatial.distance import cdist 
+import random 
+import warnings 
+warnings.filterwarnings("ignore") 
+import pickle
+from sklearn.utils.class_weight import compute_class_weight
+from xgboost import XGBClassifier  
+import dtw 
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+from ts2vec import TS2Vec
+from scipy import spatial 
+
+
+with open("BTC_USDT-4h-12.json") as f: 
+    d = json.load(f) 
+
+chart_df = pd.DataFrame(d) 
+chart_df = chart_df.rename(columns={0:"timestamp", 1:"open", 2:"high", 3:"low", 4:"close", 5:"volume"})
+
+def process(df): 
+    binance = ccxt.binance() 
+    dates = df["timestamp"].values 
+    timestamp = [] 
+    for i in range(len(dates)):
+        date_string = binance.iso8601(int(dates[i])) 
+        date_string = date_string[:10] + " " + date_string[11:-5] 
+        timestamp.append(date_string) 
+    df["datetime"] = timestamp
+    df = df.drop(columns={"timestamp"}) 
+    return df 
+
+chart_df = process(chart_df) 
+
+hours, days, months, years = [],[],[],[] 
+for dt in tqdm(chart_df["datetime"]):
+        dtobj = pd.to_datetime(dt) 
+        hour = dtobj.hour 
+        day = dtobj.day 
+        month = dtobj.month 
+        year = dtobj.year 
+        hours.append(hour) 
+        days.append(day) 
+        months.append(month) 
+        years.append(year) 
+
+chart_df["hours"] = hours 
+chart_df["days"] = days  
+chart_df["months"] = months 
+chart_df["years"] = years 
+
+def preprocess_seq_data(chart_df, threshold=0.0075): 
+    targets = [] 
+    openv = chart_df["open"].values 
+    close = chart_df["close"].values 
+    high = chart_df["high"].values 
+    low = chart_df["low"].values  
+    volume = chart_df["volume"].values 
+    
+    for i in range(close.shape[0]-1):
+        high_vol = (high[i+1] - close[i]) / close[i] 
+        low_vol = (low[i+1] - close[i]) / close[i] 
+        if high_vol >= threshold: 
+            targets.append(0) 
+        elif low_vol <= -threshold:
+            targets.append(1) 
+        else:
+            targets.append(2) 
+        
+    targets.append(None) 
+    chart_df["Targets"] = targets 
+    
+    chart_df.set_index(pd.DatetimeIndex(chart_df["datetime"]), inplace=True)
+    chart_df["bop"] = chart_df.ta.bop(lookahead=False) 
+    chart_df["ebsw"] = chart_df.ta.ebsw(lookahead=False) 
+    chart_df["cmf"] = chart_df.ta.cmf(lookahead=False) 
+    chart_df["rsi/100"] = chart_df.ta.rsi(lookahead=False) / 100
+    chart_df["high/low"] = chart_df["high"] / chart_df["low"] 
+    chart_df["high/open"] = chart_df["high"] / chart_df["open"] 
+    chart_df["low/open"] = chart_df["low"] / chart_df["open"] 
+    chart_df["close/open"] = chart_df["close"] / chart_df["open"] 
+    chart_df["high/close"] = chart_df["high"] / chart_df["close"] 
+    chart_df["low/close"] = chart_df["low"] / chart_df["close"]     
+    for l in tqdm(range(1, 12), position=0, leave=True): 
+        for col in ["open", "high", "low", "close", "volume"]:
+            val = chart_df[col].values 
+            val_ret = [None for _ in range(l)]
+            for i in range(l, len(val)):
+                if val[i-l] == 0: 
+                    ret = 1 
+                else:
+                    ret = val[i] / val[i-l]  
+                val_ret.append(ret) 
+            chart_df["{}_change_{}".format(col, l)] = val_ret 
+
+    chart_df.dropna(inplace=True) 
+    chart_df.drop(columns={"open", "high", "low", "close", "volume"}, inplace=True) 
+    return chart_df
+
+chart_df = preprocess_seq_data(chart_df) 
+train_columns = []
+for col in chart_df.columns:
+    if col not in ["Targets", "datetime", "years", "hours", "days", "months"]:
+        train_columns.append(col)  
+
+X = chart_df[train_columns] 
+Y = chart_df["Targets"] 
+
+train_size = int(chart_df.shape[0] * 0.8) 
+val_size = int(chart_df.shape[0] * 0.1) 
+
+X_train = X.iloc[:train_size] 
+Y_train = Y.iloc[:train_size] 
+
+X_val = X.iloc[train_size:train_size+val_size] 
+Y_val = Y.iloc[train_size:train_size+val_size] 
+
+X_test = X.iloc[train_size+val_size:] 
+Y_test = Y.iloc[train_size+val_size:] 
+
+# we only train using the train dataset 
+num_features = X_train.shape[1] 
+
+num_features = X_train.shape[1] 
+model = TS2Vec(input_dims=num_features, output_dims=128) 
+model.load("ts2vec_embedder_XGB.pt")
+
+start_index = train_size # meta model is trained only on the validation dataset  
+all_dates = chart_df.index 
+
+lookback = 6 
+
+date_chart_emb = {} 
+
+for i in tqdm(range(lookback, len(all_dates)), position=0, leave=True):
+    output = model.encode(X.values[i-lookback:i].reshape((-1, lookback, num_features))) 
+    output = np.mean(output, axis=1) 
+    date_chart_emb[all_dates[i]] = output 
+    
+with open("all_ts2vec_embeddings.pkl", "wb") as f: 
+    pickle.dump(date_chart_emb, f) 
+
+similar_dates = {} 
+
+def get_cosine_distance(x1, x2):
+    return spatial.distance.cosine(x1, x2)
+
+max_topk = 30 # get at most 30 similar patterns 
+
+for i in tqdm(range(start_index, chart_df.shape[0]), position=0, leave=True, desc="Total number of samples"):
+    query_date = all_dates[i] 
+    query_emb = date_chart_emb[query_date] 
+    candidate_info = [] 
+    for j in range(lookback, i): 
+        candidate_date = all_dates[j] 
+        candidate_emb = date_chart_emb[candidate_date] 
+        dist = get_cosine_distance(query_emb, candidate_emb) 
+        candidate_info.append((candidate_date, dist)) 
+    candidate_info = sorted(candidate_info, key=lambda x:x[1]) 
+    similar_dates[query_date] = candidate_info[:max_topk]  
+    
+with open("ts2vec_similar_dates.pkl", "wb") as f: 
+    pickle.dump(similar_dates, f) 
+    
+print("done!")
